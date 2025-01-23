88214ff5e9feb8da2755611cf901a72e7d844a69
Dread-Vin <salangavincent@gmail.com>
2025-01-23 15:32:09 +0800

    commented logout

--- .history/Main/TPayWeb_API_V3/Api_Auth_test.spec_20250120163711.ts
+++ .history/Main/TPayWeb_API_V3/Api_Auth_test.spec_20250120163711.ts
@@ -51,8 +51,6 @@ test.describe.parallel('API Testing - Login', () => {
     }
   });
 
-  /*
-
   test('Logout', async ({ request }) => {
     allure.description('This test logs out a user and clears the tokens from environment variables.');
 
@@ -83,7 +81,6 @@ test.describe.parallel('API Testing - Login', () => {
     setEnvVariable('secret_key', '');
     setEnvVariable('generated_totp_code', '');
   });
-  */
 });
 
 test.describe.parallel('API Testing - WhoAmI', () => {
